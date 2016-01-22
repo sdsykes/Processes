a120816d01426b08db0bb9a093a3da5516aa4d25
Stephen Sykes <sdsykes@gmail.com>
2016-01-22 22:08:45 +0200

    Initial Commit

---
+++ Processes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Processes
+//
+//  Created by Stephen Sykes on 22/01/16.
+//  Copyright © 2016 Switchstep. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
